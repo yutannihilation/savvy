829f10d09671dc39ce4e64f6db8ce6957cffba0a
Hiroaki Yutani <yutani.ini@gmail.com>
2023-07-08 19:53:30 +0900

    Implement unwind_protect()

--- src/rust/src/unwind_protect_wrapper.rs
+++ src/rust/src/unwind_protect_wrapper.rs
@@ -6,3 +6,22 @@ extern "C" {
         data: *mut ::std::os::raw::c_void,
     ) -> SEXP;
 }
+
+pub(crate) unsafe fn unwind_protect<F>(f: F) -> SEXP
+where
+    F: FnOnce() -> SEXP + Copy,
+{
+    unsafe extern "C" fn do_call<F>(data: *mut std::os::raw::c_void) -> SEXP
+    where
+        F: FnOnce() -> SEXP + Copy,
+    {
+        let data = data as *const ();
+        let f: &F = &*(data as *const F);
+        f()
+    }
+
+    let fun_ptr = do_call::<F> as *const ();
+    let fun = std::mem::transmute(fun_ptr);
+    let data = std::mem::transmute(&f as *const F);
+    unwind_protect_impl(fun, data)
+}
