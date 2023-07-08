cd4c31b1bda2b119a113c6d7080e4a49c18f5a59
Hiroaki Yutani <yutani.ini@gmail.com>
2023-07-08 23:46:02 +0900

    Tweak

--- src/init.c
+++ src/init.c
@@ -4,9 +4,9 @@
 
 static uintptr_t TAGGED_POINTER_MASK = (uintptr_t)1;
 
-SEXP unextendr_to_upper_wrapper(SEXP x)
+SEXP handle_result(SEXP res_)
 {
-  uintptr_t res = (uintptr_t)unextendr_to_upper(x);
+  uintptr_t res = (uintptr_t)res_;
 
   // An error is indicated by tag.
   if ((res & TAGGED_POINTER_MASK) == 1)
@@ -27,109 +27,37 @@ SEXP unextendr_to_upper_wrapper(SEXP x)
     }
     else
     {
-      //  In case 2, the result is the token to restart the
+      // In case 2, the result is the token to restart the
       // cleanup process on R's side.
       R_ContinueUnwind(res_aligned);
     }
   }
 
   return (SEXP)res;
+}
+
+SEXP unextendr_to_upper_wrapper(SEXP x)
+{
+  SEXP res = unextendr_to_upper(x);
+  return handle_result(res);
 }
 
 SEXP unextendr_times_two_int_wrapper(SEXP x)
 {
-  uintptr_t res = (uintptr_t)unextendr_times_two_int(x);
-
-  // An error is indicated by tag.
-  if ((res & TAGGED_POINTER_MASK) == 1)
-  {
-    // Remove tag
-    SEXP res_aligned = (SEXP)(res & ~TAGGED_POINTER_MASK);
-
-    // Currently, there are two types of error cases:
-    //
-    //   1. Error from Rust code
-    //   2. Error from R's C API, which is caught by R_UnwindProtect()
-    //
-    if (TYPEOF(res_aligned) == CHARSXP)
-    {
-      // In case 1, the result is an error message that can be passed to
-      // Rf_error() directly.
-      Rf_error("%s", CHAR(res_aligned));
-    }
-    else
-    {
-      //  In case 2, the result is the token to restart the
-      // cleanup process on R's side.
-      R_ContinueUnwind(res_aligned);
-    }
-  }
-
-  return (SEXP)res;
+  SEXP res = unextendr_times_two_int(x);
+  return handle_result(res);
 }
 
 SEXP unextendr_times_two_numeric_wrapper(SEXP x)
 {
-  uintptr_t res = (uintptr_t)unextendr_times_two_numeric(x);
-
-  // An error is indicated by tag.
-  if ((res & TAGGED_POINTER_MASK) == 1)
-  {
-    // Remove tag
-    SEXP res_aligned = (SEXP)(res & ~TAGGED_POINTER_MASK);
-
-    // Currently, there are two types of error cases:
-    //
-    //   1. Error from Rust code
-    //   2. Error from R's C API, which is caught by R_UnwindProtect()
-    //
-    if (TYPEOF(res_aligned) == CHARSXP)
-    {
-      // In case 1, the result is an error message that can be passed to
-      // Rf_error() directly.
-      Rf_error("%s", CHAR(res_aligned));
-    }
-    else
-    {
-      //  In case 2, the result is the token to restart the
-      // cleanup process on R's side.
-      R_ContinueUnwind(res_aligned);
-    }
-  }
-
-  return (SEXP)res;
+  SEXP res = unextendr_times_two_numeric(x);
+  return handle_result(res);
 }
 
 SEXP unextendr_flip_logical_wrapper(SEXP x)
 {
-  uintptr_t res = (uintptr_t)unextendr_flip_logical(x);
-
-  // An error is indicated by tag.
-  if ((res & TAGGED_POINTER_MASK) == 1)
-  {
-    // Remove tag
-    SEXP res_aligned = (SEXP)(res & ~TAGGED_POINTER_MASK);
-
-    // Currently, there are two types of error cases:
-    //
-    //   1. Error from Rust code
-    //   2. Error from R's C API, which is caught by R_UnwindProtect()
-    //
-    if (TYPEOF(res_aligned) == CHARSXP)
-    {
-      // In case 1, the result is an error message that can be passed to
-      // Rf_error() directly.
-      Rf_error("%s", CHAR(res_aligned));
-    }
-    else
-    {
-      //  In case 2, the result is the token to restart the
-      // cleanup process on R's side.
-      R_ContinueUnwind(res_aligned);
-    }
-  }
-
-  return (SEXP)res;
+  SEXP res = unextendr_flip_logical(x);
+  return handle_result(res);
 }
 
 static const R_CallMethodDef CallEntries[] = {
