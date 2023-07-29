af8f5eee14975f23aeee794602e2748fe9a7ee3d
Hiroaki Yutani <yutani.ini@gmail.com>
2023-07-29 16:03:25 +0900

    Use cbindgen

--- src/rust/api.h
+++ src/rust/api.h
@@ -5,3 +5,5 @@ SEXP unextendr_times_two_int(SEXP x);
 SEXP unextendr_times_two_numeric(SEXP x);
 
 SEXP unextendr_flip_logical(SEXP x);
+
+extern SEXP unwind_protect_impl(SEXP (*fun)(void *data), void *data);
