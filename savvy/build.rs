f2afcf406943a50fcf01bf7c5c6c0d5f3ce56ce9
Hiroaki Yutani <yutani.ini@gmail.com>
2023-09-15 23:15:08 +0900

    Tweak

--- savvy/build.rs
+++ savvy/build.rs
@@ -1,13 +1,16 @@
 use std::path::Path;
 
 fn main() {
-    let r_include_dir =
-        std::env::var("R_INCLUDE_DIR").expect("R_INCLUDE_DIR envvar must be provided.");
+    let r_include_dir = std::env::var("R_INCLUDE_DIR");
 
-    cc::Build::new()
-        .file("src/unwind_protect_wrapper.c")
-        .include(Path::new(&r_include_dir))
-        .compile("unwind_protect");
+    if let Ok(d) = r_include_dir {
+        cc::Build::new()
+            .file("src/unwind_protect_wrapper.c")
+            .include(Path::new(d.as_str()))
+            .compile("unwind_protect");
+    } else {
+        eprintln!("R_INCLUDE_DIR envvar must be provided.");
+    }
 
     println!("cargo:rerun-if-changed=src/unwind_protect_wrapper.c");
 }
