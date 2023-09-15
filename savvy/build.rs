494f449bd7b1f4c2126155969dd5729f8284ad90
Hiroaki Yutani <yutani.ini@gmail.com>
2023-09-15 23:27:40 +0900

    Update README

--- savvy/build.rs
+++ savvy/build.rs
@@ -3,13 +3,14 @@ use std::path::Path;
 fn main() {
     let r_include_dir = std::env::var("R_INCLUDE_DIR");
 
+    // TODO: to pass the build of cargo-dist, this must be built without any errors.
     if let Ok(d) = r_include_dir {
         cc::Build::new()
             .file("src/unwind_protect_wrapper.c")
             .include(Path::new(d.as_str()))
             .compile("unwind_protect");
     } else {
-        eprintln!("R_INCLUDE_DIR envvar must be provided.");
+        println!("cargo:warning=R_INCLUDE_DIR envvar should be provided.");
     }
 
     println!("cargo:rerun-if-changed=src/unwind_protect_wrapper.c");
