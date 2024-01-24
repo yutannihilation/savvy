8d4ed14bf022592b8a9a8f1ebd0578e7b1e53d78
Hiroaki Yutani <yutani.ini@gmail.com>
2024-01-25 08:45:08 +0900

    Do not use unwindprotect on wasm

--- build.rs
+++ build.rs
@@ -3,15 +3,17 @@ use std::path::Path;
 fn main() {
     let r_include_dir = std::env::var("R_INCLUDE_DIR");
 
-    // TODO: to pass the build of cargo-dist, this must be built without any errors.
-    if let Ok(d) = r_include_dir {
-        cc::Build::new()
-            .file("src/unwind_protect_wrapper.c")
-            .include(Path::new(d.as_str()))
-            .compile("unwind_protect");
-    } else {
-        println!("cargo:warning=R_INCLUDE_DIR envvar should be provided.");
-    }
+    if cfg!(not(target_arch = "wasm32")) {
+        // TODO: to pass the build of cargo-dist, this must be built without any errors.
+        if let Ok(d) = r_include_dir {
+            cc::Build::new()
+                .file("src/unwind_protect_wrapper.c")
+                .include(Path::new(d.as_str()))
+                .compile("unwind_protect");
+        } else {
+            println!("cargo:warning=R_INCLUDE_DIR envvar should be provided.");
+        }
 
-    println!("cargo:rerun-if-changed=src/unwind_protect_wrapper.c");
+        println!("cargo:rerun-if-changed=src/unwind_protect_wrapper.c");
+    }
 }
