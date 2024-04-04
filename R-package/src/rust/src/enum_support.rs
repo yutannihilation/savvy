cc2d07fe6228d02f3f092432608cafc0c0156fa3
Hiroaki Yutani <yutani.ini@gmail.com>
2024-04-05 00:30:00 +0900

    Remove tweaks

--- R-package/src/rust/src/enum_support.rs
+++ R-package/src/rust/src/enum_support.rs
@@ -1,5 +1,3 @@
-use std::ops::Deref;
-
 use savvy::{r_println, savvy};
 
 /// A Or B.
@@ -10,22 +8,6 @@ use savvy::{r_println, savvy};
 enum FooEnum {
     A = 0,
     B = 1,
-}
-
-const FOO_A: FooEnum = FooEnum::A;
-const FOO_B: FooEnum = FooEnum::B;
-
-impl TryFrom<savvy::Sexp> for &FooEnum {
-    type Error = savvy::Error;
-
-    fn try_from(value: savvy::Sexp) -> savvy::Result<Self> {
-        let i = <i32>::try_from(value)?;
-        match i {
-            0 => Ok(&FOO_A),
-            1 => Ok(&FOO_B),
-            _ => Err("Unexpected enum variant".into()),
-        }
-    }
 }
 
 #[savvy]
