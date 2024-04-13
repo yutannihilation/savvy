e66db743f3fd10c841bf783d199a4a5ad2442b6e
Hiroaki Yutani <yutani.ini@gmail.com>
2024-04-13 16:49:54 +0900

    Add tweaks

--- savvy-cli/src/main.rs
+++ savvy-cli/src/main.rs
@@ -345,7 +345,9 @@ async fn run_test(tests: String) -> std::io::Result<()> {
         &temp_r,
         &format!(
             r###"
+# TODO: Remove this tweak
 options("savvy.use_installed_cli" = TRUE)
+
 e <- new.env()
 savvy::savvy_source(r"(
 {tests}
