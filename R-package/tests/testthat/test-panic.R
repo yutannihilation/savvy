e8feddd8a28eca8578514f7c6f4892cefa9a0495
Hiroaki Yutani <yutani.ini@gmail.com>
2024-04-16 23:38:45 +0900

    Add a comment [skip ci]

--- R-package/tests/testthat/test-panic.R
+++ R-package/tests/testthat/test-panic.R
@@ -1,3 +1,6 @@
+# it seems devtools::test() re-compiles the source code with DEBUG=true, so, at
+# least on GitHub CI, this test should succeed. When this test is executed on
+# local after the package is build with release profile, this fails.
 test_that("panic doesn't crash R session", {
   expect_error(must_panic())
 })
