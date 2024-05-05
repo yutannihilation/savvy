f0c85e3531583b3de1453ce3d64ea965e5697014
Hiroaki Yutani <yutani.ini@gmail.com>
2024-05-06 01:25:42 +0900

    Tweak

--- R-package/tests/testthat/test-error_handling.R
+++ R-package/tests/testthat/test-error_handling.R
@@ -7,7 +7,7 @@ test_that("error handling works", {
   withr::with_options(list(warn = 2),
     expect_snapshot(safe_warn(), error = TRUE)
   )
-    expect_equal(get_foo_value(), 0L) # drop is properly done
+  expect_equal(get_foo_value(), 0L) # drop is properly done
 
   expect_error(raise_error(), "This is my custom error")
 })
