c02399b3ea9841efc81914df8b2699546fa17873
Hiroaki Yutani <yutani.ini@gmail.com>
2025-09-24 22:53:39 +0900

    Use bootstrap.R (#398)
    * Use bootstrap.R
    
    * Tweak
    
    * Fix

---
+++ R-package/bootstrap.R
@@ -0,0 +1,23 @@
+dep_dir <- "dep_crates"
+
+# Tweak Cargo.toml
+cargo_toml <- "src/rust/Cargo.toml"
+lines <- readLines(cargo_toml)
+writeLines(
+  gsub("../../../", "../dep_crates/", lines, fixed = TRUE),
+  cargo_toml
+)
+
+dir.create("src/dep_crates/")
+file.copy(
+  c(
+    "../src",
+    "../Cargo.toml",
+    "../build.rs",
+    "../savvy-macro",
+    "../savvy-bindgen",
+    "../savvy-ffi"
+  ),
+  "src/dep_crates/",
+  recursive = TRUE
+)
