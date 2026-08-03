e1ea6f5ba8e743df9cf7c860807a7e2fa0dc51b6
Hiroaki Yutani <yutani.ini@gmail.com>
2026-08-04 00:59:00 +0900

    Fix

--- R-package/tools/rustarch.R
+++ R-package/tools/rustarch.R
@@ -1,7 +1,11 @@
 # The original file is in the hellorust pacakge by Jeroen Ooms, which is under the MIT license.
 # cf. https://github.com/r-rust/hellorust/blob/master/tools/rustarch.R
-if(grepl("aarch", R.version$platform) || grepl("clang", Sys.getenv("R_COMPILED_BY"))){
-  cat("aarch64-pc-windows-gnullvm")
+arch <- if(grepl("aarch", R.version$platform)){
+  "aarch64-pc-windows-gnullvm"
+} else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+  "x86_64-pc-windows-gnullvm"
 } else {
-  cat("x86_64-pc-windows-gnu")
+  "x86_64-pc-windows-gnu"
 }
+
+cat(arch)
